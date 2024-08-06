4122dffd4f555a6e0a6ab23e0c8cbe39a41b7b9a
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-08-06 10:59:09 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,39 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class Main {
-  public static void main(String[] args) {
-    try {
-      ServerSocket serverSocket = new ServerSocket(4221);
-      serverSocket.setReuseAddress(true);
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
 
-      Socket socket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
+            Socket socket = serverSocket.accept(); // Wait for connection from client.
+            System.out.println("accepted new connection");
 
-      socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            OutputStream outputStream = socket.getOutputStream();
+
+            String line = reader.readLine();
+            String[] tokens = line.split(" ");
+
+            String httpMethod = tokens[0];
+            String requestTarget = tokens[1];
+            String httpVersion = tokens[2];
+
+            if (httpMethod.equals("GET")) {
+                if (requestTarget.equals("/")) {
+                    outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                } else {
+                    outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+                }
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
-  }
 }
