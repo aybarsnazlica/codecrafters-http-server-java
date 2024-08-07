bf58cb086a256a6804523cecbcd7fc6dacd31bd7
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-08-07 16:33:47 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,42 +2,52 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class Main {
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(4221)) {
             serverSocket.setReuseAddress(true);
 
-            Socket socket = serverSocket.accept(); // Wait for connection from client.
-            System.out.println("accepted new connection");
+            Socket socket = serverSocket.accept();
+            System.out.println("Accepted new connection");
 
-            InputStream inputStream = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             OutputStream outputStream = socket.getOutputStream();
 
             String line = reader.readLine();
-            String[] tokens = line.split(" ");
 
-            String httpMethod = tokens[0];
-            String requestTarget = tokens[1];
-
-            if (httpMethod.equals("GET")) {
-                if (requestTarget.equals("/")) {
-                    outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                } else if (requestTarget.startsWith("/echo/")) {
-                    String msg = requestTarget.split("/")[2];
-                    String header = String.format(
-                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-                            msg.length(),
-                            msg
-                    );
-                    outputStream.write(header.getBytes());
-                } else {
-                    outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-                }
+            if (line != null) {
+                sendResponse(line, reader, outputStream, socket);
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    private static void sendResponse(String line, BufferedReader reader, OutputStream outputStream, Socket socket) throws IOException {
+        String[] tokens = line.split(" ");
+        String httpMethod = tokens[0];
+        String requestTarget = tokens[1];
+        String httpVersion = tokens[2];
+        String response;
+
+        if ("GET".equals(httpMethod)) {
+            if ("/".equals(requestTarget)) {
+                response = String.format("%s 200 OK\r\n\r\n", httpVersion);
+            } else if (requestTarget.startsWith("/user-agent")) {
+                reader.readLine(); // Read and ignore the second line
+                String line3 = reader.readLine();
+                String[] tokens3 = line3.split(" ");
+                String responseBody = tokens3[1];
+                response = String.format("%s 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", httpVersion, responseBody.length(), responseBody);
+            } else if (requestTarget.startsWith("/echo")) {
+                String msg = requestTarget.split("/")[2];
+                response = String.format("%s 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", httpVersion, msg.length(), msg);
+            } else {
+                response = String.format("%s 404 Not Found\r\n\r\n", httpVersion);
+            }
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+            socket.close();
         }
     }
 }
